b1c66caeeea10b21e3658302d6805f275a7d8552
huy8895 <huy8895@gmail.com>
2020-07-15 20:38:39 +0700

    Đảo ngược phần tử trong mảng số nguyên sử dụng Stack

---
+++ src/stackdaonguocphantu/ReverseIntegerArray.java
@@ -0,0 +1,22 @@
+package stackdaonguocphantu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class ReverseIntegerArray {
+    public static void main(String[] args) {
+        int [] arrInt = new int[]{1,3,4,12,5};
+
+        Stack<Integer> stack = new Stack<Integer>();
+        for (Integer integer : arrInt)
+            stack.push(integer);
+
+        for (int i = 0; i < arrInt.length;i ++){
+            arrInt[i] = stack.pop();
+        }
+
+        System.out.println(Arrays.toString(arrInt));
+
+    }
+}
