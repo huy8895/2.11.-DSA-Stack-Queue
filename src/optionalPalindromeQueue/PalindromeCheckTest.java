cfb64ad0f3174e191775720f77da879af4b7979f
huy8895 <huy8895@gmail.com>
2020-07-16 10:41:00 +0700

    [Optional] [Bài tập] Kiểm tra chuỗi Palindrome sử dụng Queue

---
+++ src/optionalPalindromeQueue/PalindromeCheckTest.java
@@ -0,0 +1,10 @@
+package optionalPalindromeQueue;
+
+public class PalindromeCheckTest {
+    public static void main(String[] args) {
+        PalindromeCheck check = new PalindromeCheck();
+
+        System.out.println(check.isElementStackVsQueueEquals());
+
+    }
+}
