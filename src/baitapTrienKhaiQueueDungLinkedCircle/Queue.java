14eb838f1af28e4037e9415abd633e5cf7f56833
huy8895 <huy8895@gmail.com>
2020-07-17 23:44:53 +0700

    [Bài tập] Triển khai Queue sử dụng danh sách liên kết vòng

--- src/baitapTrienKhaiQueueDungLinkedCircle/Queue.java
+++ src/baitapTrienKhaiQueueDungLinkedCircle/Queue.java
@@ -4,24 +4,88 @@ public class Queue {
     Node front;
     Node rear;
 
+    public Node getFront() {
+        return front;
+    }
 
-    class Node{
+    public void setFront(Node front) {
+        this.front = front;
+    }
+
+    public Node getRear() {
+        return rear;
+    }
+
+    public void setRear(Node rear) {
+        this.rear = rear;
+    }
+
+    public Queue() {
+        this.front = null;
+        this.rear = null;
+    }
+
+    class Node {
         int data;
         Node link;
 
-        void Node (int data){
+        public Node(int data) {
             this.data = data;
             this.link = null;
+
         }
-    }
 
-    void enQueue(int data){
 
     }
-    void deQueue(){
+
+
+    void enQueue(int data) {
+        Node temp = new Node(data);
+        if (isEmpty()) {
+            this.front = temp;
+            this.rear = temp;
+        } else {
+            temp.link = this.front;
+            this.rear.link = temp;
+            this.rear = temp;
+
+        }
 
     }
-    void displayQueue(){
 
+    void deQueue() {
+        if (isEmpty()){
+            System.out.println("null");
+            return;
+        }
+        if (this.front == this.rear){
+            this.front = this.rear = null;
+        } else {
+            this.front = this.front.link;
+            this.rear.link = this.front;
+        }
+
+    }
+
+    void displayQueue() {
+        Node temp = this.front;
+        if (isEmpty()){
+            System.out.println("null");
+            return;
+        }
+
+        StringBuilder result = new StringBuilder();
+        result.append("[" + front.data);
+        while (temp.link!= this.front){
+            result.append(" , ").append(temp.link.data);
+            temp = temp.link;
+        }
+        result.append("]");
+        System.out.println(result.toString());
+
+    }
+
+    boolean isEmpty() {
+        return (this.front == null);
     }
 }
--- src/baitapTrienKhaiQueueDungLinkedCircle/QueueTest.java
+++ src/baitapTrienKhaiQueueDungLinkedCircle/QueueTest.java
@@ -1,4 +1,23 @@
 package baitapTrienKhaiQueueDungLinkedCircle;
 
 public class QueueTest {
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        System.out.println(queue.isEmpty());
+        queue.displayQueue();
+
+        queue.enQueue(10);
+        queue.enQueue(11);
+        //queue.displayQueue();
+        queue.enQueue(12);
+        queue.displayQueue();
+        queue.deQueue();
+        queue.displayQueue();
+        queue.deQueue();
+        queue.displayQueue();
+        System.out.println(queue.getFront().data);
+        System.out.println(queue.getRear().data);
+    }
+
 }
+
